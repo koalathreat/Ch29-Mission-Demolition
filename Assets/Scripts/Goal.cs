7ed27d0c332693cb664879996317bb82967bc760
koalathreat <edwin.zheng@rutgers.edu>
2017-10-06 21:40:05 -0400

    Final Version of Mission Demolition
    Added materials, goal, clouds, slingshots, and ground. Then added in the
    scripts needed.

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+    static public bool goalMet = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Projectile")
+        {
+            Goal.goalMet = true;
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = 1;
+            GetComponent<Renderer>().material.color = c;
+        }    
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
